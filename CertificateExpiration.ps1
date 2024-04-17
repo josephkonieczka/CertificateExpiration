378abd5f5e31346f29a382e15014e2b9d7552bad
josephkonieczka <15926710+josephkonieczka@users.noreply.github.com>
2024-04-17 08:34:10 -0500

    Update CertificateExpiration.ps1

--- CertificateExpiration.ps1
+++ CertificateExpiration.ps1
@@ -1,8 +1,9 @@
 $today = Get-Date
 $certFriendlyName = 'CERT_NAME'
 #Recursion gets all of the certificates but there could be multiple ones if full path not specified
-#$allCertificates = Get-ChildItem -Path Cert:\LocalMachine\ -Recurse
-$allCertificates = Get-ChildItem -Path Cert:\LocalMachine\Root
+$allCertificates = Get-ChildItem -Path Cert:\LocalMachine\ -Recurse
+$allCertificates = Get-ChildItem -Path Cert:\LocalMachine\ -Recurse | Select-Object -First 1
+#$allCertificates = Get-ChildItem -Path Cert:\LocalMachine\Root
 $selectedCert = $allCertificates `
 | Where-Object { $_.FriendlyName -eq $certFriendlyName } `
 | Select-Object -First 1
